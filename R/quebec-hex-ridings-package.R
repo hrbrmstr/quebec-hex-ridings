8e55ec55a902248ed37d2ca64de617d74ae1b1d1
hrbrmstr <bob@rud.is>
2018-10-09 11:18:31 -0400

    R package repo initialization complete

---
+++ R/quebec-hex-ridings-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/quebec-hex-ridings>
+#' - BugReports: <https://gitlab.com/hrbrmstr/quebec-hex-ridings/issues>
+#' 
+#' @md
+#' @name quebec-hex-ridings
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
